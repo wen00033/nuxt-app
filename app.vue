0e696908972ed720126e9eb50688ddeb7034513a
Chi-hung Wen <wen00033@algonquinlive.com>
2024-03-09 17:38:59 -0500

    Update devtools and add Header component

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Header />
   </div>
 </template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+ul,
+ol {
+  list-style: none;
+}
+
+body {
+  font-family: sans-serif;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li v-for="(el, i) in navigation" :key="i">
+          {{ el }}
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const navigation = ref(["Home", "About", "Contact"]);
+</script>
+
+<style lang="scss">
+header {
+  background-color: #333;
+  color: white;
+  padding: 2rem;
+  ul {
+    display: flex;
+    padding-inline: 10%;
+    justify-content: flex-end;
+    flex-direction: row;
+    gap: 1rem;
+    li {
+      font-size: 2rem;
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
