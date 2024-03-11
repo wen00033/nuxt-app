e69108ae80d0b0245fcc519cd5cbf7b025def274
Chi-hung Wen <wen00033@algonquinlive.com>
2024-03-11 14:51:41 -0400

    Add target and content configuration to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: "static",
+  content: {
+    dir: "content",
+  },
+  buildModules: ["@nuxt/content"],
   ssr: true,
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt", "@nuxt/ui", "@nuxt/content"],
