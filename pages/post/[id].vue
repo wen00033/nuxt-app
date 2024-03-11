ee9647824d43ad8af798d3cb952636e0dcbc269f
Chi-hung Wen <wen00033@algonquinlive.com>
2024-03-11 11:25:13 -0400

    Refactor styles and add new post page

---
+++ pages/post/[id].vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLayout name="default">
+    <!-- <template #title>title</template> -->
+    <template #content>
+      <ContentDoc v-slot="{ doc }">
+        <h1 class="pageTitle">{{ doc.title }}</h1>
+        <p>
+          Post Date: <strong>{{ doc.date.slice(0, 10) }}</strong>
+        </p>
+        <div class="blog-container">
+          <img class="blog-img" :src="doc.thumbnail" alt="image" />
+          <p class="blog-paragraph">
+            <ContentRenderer :value="doc"></ContentRenderer>
+          </p>
+        </div>
+      </ContentDoc>
+    </template>
+    <p>Post ID: {{ route.params.id }}</p>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<style lang="scss"></style>
+import type { Title } from '#build/components';
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -2,7 +2,22 @@
   <NuxtLayout name="default">
     <template #title> News </template>
     <template #content>
-      <h2>news page</h2>
+      <ContentNavigation v-slot="{ navigation }" :query="postQuery">
+        <ul class="news-container" v-for="(post, i) in navigation" :key="i">
+          <li v-for="(link, i) in post.children" :key="i">
+            <NuxtLink :to="link._path">
+              <h2 class="menuTitle">
+                {{ link.title }}
+              </h2>
+            </NuxtLink>
+          </li>
+        </ul>
+      </ContentNavigation>
+      <!-- <ContentDoc /> -->
     </template>
   </NuxtLayout>
 </template>
+
+<script setup>
+const postQuery = queryContent("post");
+</script>
