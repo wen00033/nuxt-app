848d9987263642e612e5ab5d134d55693cd3203e
Chi-hung Wen <wen00033@algonquinlive.com>
2024-03-11 13:40:14 -0400

    Enable SSR and remove unnecessary configuration in nuxt.config.ts file. Update backend configuration in public/admin/config.yml file.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt", "@nuxt/ui", "@nuxt/content"],
   colorMode: {
     preference: "light",
   },
-  target: "static",
 });
