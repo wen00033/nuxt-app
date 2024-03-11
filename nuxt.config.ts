137a878aa58ec7a225d17fe77d69767d64465345
Chi-hung Wen <wen00033@algonquinlive.com>
2024-03-11 13:26:19 -0400

    Add target: "static" to nuxt.config.ts and remove unused code in [id].vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,5 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "light",
   },
+  target: "static",
 });
--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -21,8 +21,4 @@
 
 <script setup>
 const route = useRoute();
-console.log(route.params.id);
 </script>
-
-<style lang="scss"></style>
-import type { Title } from '#build/components';
