a4346ef40577c3dbfc5acc8ee982bd682992fc90
Chi-hung Wen <wen00033@algonquinlive.com>
2024-03-12 13:37:13 -0400

    Refactor ContentList component in starter page

--- pages/starter/index.vue
+++ pages/starter/index.vue
@@ -2,19 +2,25 @@
   <NuxtLayout name="default">
     <template #title> starter </template>
     <template #content>
-      <ContentList path="/starter" v-slot="{ list }">
-        <ul class="menu-item-container">
-          <li class="menu-item" v-for="(starter, i) in list" :key="i">
-            <img class="menu-image" :src="starter.thumbnail" alt="starter" />
-            <div>
-              <p class="MenuTitle">{{ starter.title }}</p>
-              <h3>
-                <em>{{ starter.description }}</em>
-              </h3>
-            </div>
-            <p class="pageTitle">${{ starter.price }}</p>
-          </li>
-        </ul>
+      <ContentList path="/starter">
+        <template #default="{ list }">
+          <ul class="menu-item-container">
+            <li class="menu-item" v-for="(starter, i) in list" :key="i">
+              <img class="menu-image" :src="starter.thumbnail" alt="starter" />
+              <div>
+                <p class="MenuTitle">{{ starter.title }}</p>
+                <h3>
+                  <em>{{ starter.description }}</em>
+                </h3>
+              </div>
+              <p class="pageTitle">${{ starter.price }}</p>
+            </li>
+          </ul>
+        </template>
+
+        <template #not-found>
+          <p class="pageTitle">Please add new in the list</p>
+        </template>
       </ContentList>
     </template>
   </NuxtLayout>
